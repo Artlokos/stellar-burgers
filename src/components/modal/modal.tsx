c163b55adb64bdb080d1c473e78c87e2d4ea9538
ArtLokos <artlokos@yandex.ru>
2024-12-24 01:44:01 +1000

    afterBrokenPullRequest1

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -8,15 +8,6 @@ import { useLocation } from 'react-router-dom';
 const modalRoot = document.getElementById('modals');
 
 export const Modal: FC<TModalProps> = memo(({ title, onClose, children }) => {
-  const location = useLocation();
-  const [titleStyle, setTitleStyle] = useState('text_type_main-large');
-
-  useEffect(() => {
-    if (/feed|profile/i.test(location.pathname)) {
-      setTitleStyle('text_type_digits-default');
-    }
-  });
-
   useEffect(() => {
     const handleEsc = (e: KeyboardEvent) => {
       e.key === 'Escape' && onClose();
