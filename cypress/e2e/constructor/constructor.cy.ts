bccca62f799cdafe445e22762962cecf133c3310
ArtLokos <artlokos@yandex.ru>
2024-12-28 18:14:51 +1000

    forReview

---
+++ cypress/e2e/constructor/constructor.cy.ts
@@ -0,0 +1,66 @@
+import { URL } from '@api';
+import { deleteCookie, setCookie } from '../../../src/utils/cookie';
+
+
+describe('Бургеры', () => {
+  beforeEach(() => {
+    setCookie('accessToken', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZjBhMDAyOTdlZGUwMDAxZDA2MDg1NCIsImlhdCI6MTcxMjMxMDE2NiwiZXhwIjoxNzEyMzExMzY2fQ.v7kdecJvLfdmlBsvf_BySvsfnXX3K0Er__GNYw-NRLM');
+    localStorage.setItem('refreshToken', '9cbdd5b777edfb92bd9183a7cf2372a12b545c045a9796f94c1afd0b9d374a8794aa15bee20a7556');
+    cy.intercept('GET', `${URL}//auth/user`, {fixture: 'user.json'}).as('getUser');
+    cy.intercept('GET', `${URL}/ingredients`, {fixture: 'ingredients.json'}).as('getIngredients');
+    cy.visit('');
+    cy.wait('@getUser');
+  });
+  afterEach(() => {
+    deleteCookie('accessToken');
+    localStorage.removeItem('refreshToken');
+  });
+  it('список ингредиентов с сервера', () => {
+    cy.get('[data-cy="constructor"]').as('constructor');
+
+    cy.addIngredient('Булки');
+    cy.addIngredient('Начинки');
+
+    cy.get('@constructor').should('contain', 'Флюоресцентная булка R2-D3');
+    cy.get('@constructor').should('contain', 'Говяжий метеорит (отбивная)');
+  });
+  it('открыть/закрыть модалку', () => {
+    cy.get('[data-cy="ingredient-item"]').first().click();
+    cy.get('[data-cy="modal"]').as('modal');
+    cy.get('@modal').should('exist');
+    cy.get('@modal').should('contain', 'Краторная булка N-200i');
+
+    cy.get('[data-cy="modal-close"]').click();
+    cy.get('@modal').should('not.exist');
+
+    cy.get('[data-cy="ingredient-item"]').first().click();
+    cy.get('@modal').should('exist');
+
+    cy.get('[data-cy="modal-overlay"]').click('left', {force: true});
+    cy.get('@modal').should('not.exist');
+  });
+  it('заказ', () => {
+    cy.intercept('POST', `${URL}/orders`, {fixture: 'order.json'}).as('orderBurgerApi');
+    cy.get('[data-cy="constructor"]').as('constructor');
+
+
+    cy.addIngredient('Булки');
+    cy.addIngredient('Начинки');
+
+    cy.get('@constructor').children('div').children('button').click();
+
+    cy.get('[data-cy="modal"]').as('modal');
+    cy.get('@modal').should('exist');
+    cy.get('@modal').should('contain', '37865');
+
+    cy.get('[data-cy="modal-close"]').click();
+    cy.get('@modal').should('not.exist');
+
+
+    cy.get('@constructor').should('not.contain', 'Флюоресцентная булка R2-D3');
+    cy.get('@constructor').should('not.contain', 'Плоды Фалленианского дерева');
+
+    cy.wait('@orderBurgerApi');
+
+  })
+});
